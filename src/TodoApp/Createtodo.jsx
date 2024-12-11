9f6d4c8eaab5f05a648bb0ad47453941a254eaea
Baibhav Tiwari <97496657+lexendx@users.noreply.github.com>
2024-12-11 23:34:34 +0530

    added to do app

--- src/TodoApp/Createtodo.jsx
+++ src/TodoApp/Createtodo.jsx
@@ -6,6 +6,9 @@ const Createtodo=()=> {
     <div id ={style.Createtodo}>
         <form onSubmit ={Createtodo}>
             <h2>Create todo</h2>
+            <input type ="text" placeholder="todo....." value={todo} onChange={handleTodo}/>
+            <br/>
+            <button/>
 
         </form>
 
