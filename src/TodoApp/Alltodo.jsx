505251a809339f7f610d84181f642c9d914193a4
Baibhav Tiwari <97496657+lexendx@users.noreply.github.com>
2024-12-11 23:32:15 +0530

    todo app

---
+++ src/TodoApp/Alltodo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Alltodo =()=> {
+  return (
+    <div></div>
+  )
+}
+
+export default Alltodo
---
+++ src/TodoApp/Createtodo.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+const Createtodo=()=> {
+  return (
+    <div id ={style.Createtodo}>
+        <form onSubmit ={Createtodo}>
+            <h2>Create todo</h2>
+
+        </form>
+
+    </div>
+  )
+}
+
+export default Createtodo
