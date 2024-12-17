8e36d373b5d163ed80f86236bc91341a5dddfeb4
Baibhav Tiwari <97496657+lexendx@users.noreply.github.com>
2024-12-18 01:06:37 +0530

    sss

--- src/LifeCycle/LifeCycle.jsx
+++ src/LifeCycle/LifeCycle.jsx
@@ -2,16 +2,33 @@ import React, { Component } from 'react'
 
 export default class LifeCycle extends Component {
     state={value:0};
+    //sbse pehle constructor hi initialize hoga aur ui pr render hoga
+    constructor(){
+        super()
+        console.log("I am Constructor");
+        this.state={value:0};
+    }
+
     componentDidMount(){
         console.log("Mounting phase");
+            this.s1=setInterval(()=>{
+console.log("hello");
+            },2000);
     }
     componentDidUpdate(){
         console.log("Updating Phase");
     }
+    getSnapshotBeforeUpdate(props,state){
+console.log("i am Snapshotbeforeupdatr",state);
+return null;
+    }
     componentWillUnmount(){
         console.log("Unmounting Phase");
+        clearInterval(this.s1);
     }
+    
   render() {
+    console.log("I am render");
     return (
       <div>
 
