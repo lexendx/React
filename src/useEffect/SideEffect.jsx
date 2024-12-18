cc4daaceb1c1beb9de7217eda2a775e2cb6a3fe9
Baibhav Tiwari <97496657+lexendx@users.noreply.github.com>
2024-12-18 15:29:20 +0530

    minar changes

--- src/useEffect/SideEffect.jsx
+++ src/useEffect/SideEffect.jsx
@@ -1,14 +1,22 @@
 import {useEffect} from 'react'
 
 const SideEffect=()=> {
+    
     //Syntax of useEffect
     // useEffect(()=>{},[]);
+    //render
     useEffect(()=>{
         console.log("render");
     });
+    //componentDidMount
     useEffect(()=>{
         console.log("COMPONENT MOUNTED")
     },[]);
+//componentDidUpdated
+    useEffect(()=>{
+        console.log("COMPONENT Updated")
+    },[]);
+
   return (
     <div>
         <h1>SideUffect</h1>
